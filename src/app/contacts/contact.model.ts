a0e029b63c898d0349ff013f9722910266ce6646
BarkerEmily <emilybarker@byui.edu>
2024-01-20 14:44:11 -0500

    contacts list still working on adding the name and image. NgFor not working but at this point all of the code is running correctly. ngFor commented out.

--- src/app/contacts/contact.model.ts
+++ src/app/contacts/contact.model.ts
@@ -1,11 +1,10 @@
-export class contact {
+export class Contact {
     constructor(
         public id: string, 
         public name: string, 
         public email: string,
         public phone: string,
         public imageUrl: string,
-        public group?: contact[]) {
-            
+        public group?: Contact[] | null) {
     }
 }
